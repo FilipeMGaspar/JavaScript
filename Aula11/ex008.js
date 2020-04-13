b1612c69c5cf67f021613fb8e2dc6f4cbcb1c5b5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-13 11:29:13 +0100

    aula11 - Execução condicional if

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,5 +1,7 @@
 console.log('O console funcionou correctamente');
 var vel = 12;
 console.log(`A velocidade do seu carro é ${vel}`);
-console.log('Ultrapassou a Velocidede permitida, VAI SER MULTADO!');
+if (vel > 60){
+    console.log('Ultrapassou a Velocidede permitida, VAI SER MULTADO!');
+}
 console.log('Conduza sempre com cinto de segurança');
