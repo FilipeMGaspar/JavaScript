f251e6f3d4da70ccf874458ced8df5bea56eb20f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-13 11:43:36 +0100

    aula11 - Execução Condicional IF ELSE

---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA';
+console.log(`Você Vive em ${pais}`);
+if (pais == 'Portugal'){
+    console.log(`Você vive em ${pais} é Português`);
+}else{
+    console.log(`Voce vive ${pais} é Estrageiro`);
+}
