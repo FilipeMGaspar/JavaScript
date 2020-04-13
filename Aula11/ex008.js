1f9d4c2e1b7a7aece6e9433b465d226f26f75fdb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-13 11:20:53 +0100

    aula11 -  Execução Sequencial

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,4 +1,5 @@
 console.log('O console funcionou correctamente');
 var vel = 60.5;
 console.log(`A velocidade do seu carro é ${vel}`);
+console.log('Ultrapassou a Velocidede permitida, VAI SER MULTADO!');
 console.log('Conduza sempre com cinto de segurança');
