d7c4f8c17df8fe9873f5e6be94b765cf4155f43f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-14 19:33:36 +0100

    Exercicio - Aula12 - IMG e fundo Dinâmicos

--- Aula12_Ex/ex014/script.js
+++ Aula12_Ex/ex014/script.js
@@ -3,17 +3,20 @@ function carregar(){
     var img = window.document.getElementById('imagem');
     var data = new Date();
    // var hora = data.getHours();
-   var hora =13;
+   var hora =22;
     msg.innerHTML=`Agora são <strong>${hora}</strong> horas`;
     if(hora>=0 && hora<12){
         //Bom Dia
         img.src ='imgs/manha.png'; 
+        document.body.style.background='#d4c59a';//muda a cor de fundo do corpo da página html
+        
     }else if(hora>12 && hora<=18){
         //Boa Tarde
         img.src='imgs/tarde.png';
+        document.body.style.background= '#f0a249';//muda a cor de fundo do corpo da página html
     }else{
         // Boa Noite
         img.src='imgs/noite.png';
-        
+        document.body.style.background='#35263f';
     }
 }
