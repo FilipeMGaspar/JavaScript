b296453a2ce6ebe00195dfc0a7f14ecb2bfdb6e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-14 19:23:20 +0100

    Exercico - Aula12 - IMG Dinâmicas

--- Aula12_Ex/ex014/script.js
+++ Aula12_Ex/ex014/script.js
@@ -1,4 +1,19 @@
-var msg = window.document.getElementById('msg');
-var img = window.document.getElementById('imagem');
-var data = new Date();
-var hora = data.getHours();
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+   // var hora = data.getHours();
+   var hora =13;
+    msg.innerHTML=`Agora são <strong>${hora}</strong> horas`;
+    if(hora>=0 && hora<12){
+        //Bom Dia
+        img.src ='imgs/manha.png'; 
+    }else if(hora>12 && hora<=18){
+        //Boa Tarde
+        img.src='imgs/tarde.png';
+    }else{
+        // Boa Noite
+        img.src='imgs/noite.png';
+        
+    }
+}
