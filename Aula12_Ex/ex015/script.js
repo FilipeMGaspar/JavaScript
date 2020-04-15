f809180c3e76786c5883b8fe727e70d345b0590f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-15 18:56:25 +0100

    Exercicio - aula12 - Validacao idade

--- Aula12_Ex/ex015/script.js
+++ Aula12_Ex/ex015/script.js
@@ -9,38 +9,54 @@ function validar(){
         var fsex = document.getElementsByName('radsex');//recebe o valor que vem do radiobutton
         var idade = ano - Number(fano.value);
         var gen = '';
-        var imagem = document.createElement('img');
-        imagem.setAttribute('id','foto');
+        var imagem = document.createElement('img');//cria um elemento de imagem dinâmicamente no html
+        imagem.setAttribute('id','foto');//atribui o id="foto" <img src="" id=foto>
         if (fsex[0].checked){//
             gen = 'Masculino';
             if (idade>0 && idade<=2){
                 //Bébe
+                imagem.setAttribute('src', 'imgs/bebeM.png');
             }else if(idade<12){
                 //criança
+                imagem.setAttribute('src','imgs/criancaM.png');
             }else if(idade<18){
                 //Adolecente
+                imagem.setAttribute('src','imgs/adolecenteM.png');
+            }else if(idade <30){
+                //jovem adulto
+
             }else if(idade<65){
                 //Adulto
+                imagem.setAttribute('src','imgs/adultoM.png')
             }else{
                 //idoso
+                imagem.setAttribute('src','imgs/idosoM.png')
             }
 
         }else if (fsex[1].checked){//
             gen = 'Feminino';
             if (idade>0 && idade<=2){
                 //Bébe
+                imagem.setAttribute('src','imgs/bebeF.png');
             }else if(idade<12){
                 //criança
+                imagem.setAttribute('src','imgs/criancaF.png');
             }else if(idade<18){
                 //Adolecente
+                imagem.setAttribute('src','imgs/adolecenteF.png');
+            }else if(idade <30){
+                //jovem Adulto
             }else if(idade<65){
                 //Adulto
+                imagem.setAttribute('src','imgs/adultoF.png')
             }else{
                 //idoso
+                imagem.setAttribute('src','imgs/idosoF.png');
             }
         }   
         result.style.textAlign = 'center';      
         result.innerHTML = `Você tem ${idade} anos! E é do género ${gen}!`
+        result.appendChild(imagem);//o appendChild adiciona o contúdo abaixo da informação anterior
     }   
 
 }
