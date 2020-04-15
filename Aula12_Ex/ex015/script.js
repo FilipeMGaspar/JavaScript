48ce86851414f605a58ef011aac68df19a60860c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-15 18:23:11 +0100

    Exercicio - aula12 - IF ELSE verificações

--- Aula12_Ex/ex015/script.js
+++ Aula12_Ex/ex015/script.js
@@ -1,16 +1,21 @@
 function validar(){
-    var data = new Date();
-    var ano = data.getFullYear();
-    var fano = document.getElementById('txtano');
-    var result = document.querySelector('div#result');
+    var data = new Date();//cria uma variavel do tipo data
+    var ano = data.getFullYear();//Obtém o ano completo ex: 2020
+    var fano = document.getElementById('txtano');//Obtém o conteúdo da caixa de texto
+    var result = document.querySelector('div#result');//obtém o conteúdo da div Resultado
 
     if(fano.value.length == 0 || Number(fano.value)> ano){
         alert('[ERRO!] Verifique os dados e tente novamente');     
     }else {
-        var fsex = document.getElementsByName('radsex')
+        var fsex = document.getElementsByName('radsex');//recebe o valor que vem do radiobutton
         var idade = ano - Number(fano.value);
-        
-        //result.innerHTML = `Você tem ${idade} anos`
+        var gen = '';
+        if (fsex[0].checked){//
+            gen = 'Masculino';
+        }else if (fsex[1].checked){//
+            gen = 'Feminino';
+        }        
+        //result.innerHTML = `Você tem ${idade} anos! E é do género ${gen}!`
     }   
 
 }
