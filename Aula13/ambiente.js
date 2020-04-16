1aceab3d9b2d41b949384d009d70f2f4176f957f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-16 16:56:08 +0100

    Aula12 - do{ }While - Teste lógico no fim

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,9 +1,19 @@
 var contador = 1;
-while(contador <= 6){
+do{//Estrutura de repetição com teste lógico no FIM
+    //console.log('Tudo bem!');
+    console.log(`Passo ${contador}`);
+    contador ++;//contador = contador+1
+}while(contador<=6);
+
+/*
+var contador = 1;
+while(contador <= 6){//Estrutura de repetição com teste lógico no inicio
     //console.log('Tudo bem!');
     console.log(`Passo ${contador}`);
     contador ++;//contador = contador+1
 }
+*/
+
 /*
 console.log('Tudo bem!');
 console.log('Tudo bem!');
