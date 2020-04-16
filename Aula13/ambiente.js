287e14df63e973110591f1ba7ffe0f42c2abbd3e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-16 16:53:09 +0100

    Aula12 - While(teste lógico) - no inicio

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,6 +1,7 @@
 var contador = 1;
 while(contador <= 6){
-    console.log('Tudo bem!');
+    //console.log('Tudo bem!');
+    console.log(`Passo ${contador}`);
     contador ++;//contador = contador+1
 }
 /*
