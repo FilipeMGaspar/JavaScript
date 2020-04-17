df87190e58982e280a21ceb5bd03cad1cb8e7a1d
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 11:52:29 +0100

    Aula11 - Execução Condicional Simples

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,7 +1,7 @@
-console.log('O console funcionou correctamente');
-var vel = 70;
-console.log(`A velocidade do seu carro é ${vel}`);
+console.log('O console funcionou correctamente')
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}`)
 if (vel > 60){//Condição Simples
-    console.log('Ultrapassou a Velocidede permitida, VAI SER MULTADO!');
+    console.log('Ultrapassou a Velocidede permitida, VAI SER MULTADO!')
 }
-console.log('Conduza sempre com cinto de segurança');
+console.log('Conduza sempre com cinto de segurança')
