f153cccd9376abc57af06aecd79e0ddd7db3ed8b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 16:58:24 +0100

    Exercicio - Aula14 - Ciclos de Repetição

--- Aula14EX/ex016/script.js
+++ Aula14EX/ex016/script.js
@@ -12,28 +12,35 @@ function contar(){
     let result = document.getElementById('result')//recebe o elemento <div< com id result
     
     if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0){
-        alert('[ERRO] Estão em falta DADOS!')
+        //alert('[ERRO] Estão em falta DADOS!')
+        result.innerHTML = '[ERRO] Impossivel Contar. Faltam dados'
     }else{
-        result.innerHTML = 'Estou a Contar...'
+        result.innerHTML = 'Estou a Contar:<br>'
         let inicio = Number(txtini.value)//conversão para valor numérico 
         let fim = Number(txtfim.value)
         let passo = Number(txtpasso.value)
 
         //utilizando o ciclo for
+
+        if(passo <= 0){
+            alert('[ERRO] Valor de Passo Incorrecto! Vou considerar ->Passo = 1')
+            passo = 1
+        }
+
         if(inicio < fim){
-            for(let conta = inicio; conta <= fim; conta+=passo){
+            for(let conta = inicio; conta <= fim; conta+=passo){//contagem crecente
                 result.innerHTML += ` ${conta} \u{1F449} `//    \u{1F601    Formatação unicode pra o JavaScript
             }
-            result.innerHTML += `\u{1F3C1}`
         }else {           
-            for (let cont = inicio; cont >= fim; cont -= passo){
+            for (let cont = inicio; cont >= fim; cont -= passo){//contagem decrecente
                 result.innerHTML += ` ${cont} \u{1F449} `
-            }
-            result.innerHTML += `\u{1F3C1}`
-        }     
+            }           
+        }
+        result.innerHTML += `\u{1F3C1}`     
 
         //utilizando o ciclo while
-         /* if(txtini.value.length == 0 ){
+
+         /* if(txtini.value.length == 0 || txtfim.value.length == 0 || txtpasso.value.length == 0){
             alert('![ERRO]! Estão em falta DADOS!')
           }else{
               let inicio = Number(txtini.value)
