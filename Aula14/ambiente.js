9a652b9ef8f721f4398f69d0f97ec493b71cf10f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 12:05:58 +0100

    Aula14 - Ciclo de repetição FOR

--- Aula14/ambiente.js
+++ Aula14/ambiente.js
@@ -1,13 +1,13 @@
-var c = 1;
+var c = 1
 
 /*
 while (c<=5){//ciclo de de Repetição com teste lógico no Inicip 
-    console.log(`Vale ${c}`);
-    c++;
+    console.log(`Vale ${c}`)
+    c++
 }
 */
-console.log('Vai Iniciar');
+console.log('Vai Iniciar')
 for (var c=1; c<=5; c++){//Ciclo de repetição For
-    console.log(c);
+    console.log(c)
 }
-console.log('Terminou');
+console.log('Terminou')
