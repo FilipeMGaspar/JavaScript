39e1a9e22138c933b7726ff1e35f6b5ad6edd6c5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 16:46:51 +0100

    Exercicio - Aula14 - Ciclos de Repetição

--- Aula14EX/ex016/script.js
+++ Aula14EX/ex016/script.js
@@ -20,49 +20,48 @@ function contar(){
         let passo = Number(txtpasso.value)
 
         //utilizando o ciclo for
-        for(let conta= inicio; conta<= fim; conta+=passo){
-            result.innerHTML += ` ${conta} \u{1F449} `//\u{1F601    Formatação unicode pra o JavaScript
-        }
-        result.innerHTML += `\u{1F3C1}`
-        /*
-        utilizando o while
-            if (inicio < fim){
+        if(inicio < fim){
+            for(let conta = inicio; conta <= fim; conta+=passo){
+                result.innerHTML += ` ${conta} \u{1F449} `//    \u{1F601    Formatação unicode pra o JavaScript
+            }
+            result.innerHTML += `\u{1F3C1}`
+        }else {           
+            for (let cont = inicio; cont >= fim; cont -= passo){
+                result.innerHTML += ` ${cont} \u{1F449} `
+            }
+            result.innerHTML += `\u{1F3C1}`
+        }     
+
+        //utilizando o ciclo while
+         /* if(txtini.value.length == 0 ){
+            alert('![ERRO]! Estão em falta DADOS!')
+          }else{
+              let inicio = Number(txtini.value)
+              let fim = Number(txtfim.value)
+              let passo = Number(txtpasso.value)
+              
+              if(passo == 0){//no caso do passo ser indicado como ser será considerado 1
+                    passo = 1
+                    alert('O passo Indicado 0! Será Considerado 1.')
+              }
+              
+              if (inicio < fim){
+            
                 while (inicio <= fim){                      
                     result.innerHTML += `${inicio} \u{1F449} `
                     inicio = inicio + passo  
                 }
                 result.innerHTML += `\u{1F3C1}`
+            
             }else{
+                
                 while(fim <= inicio){           
                     result.innerHTML += `${inicio} \u{1F449} `
                     inicio = inicio - passo
                 }
                 result.innerHTML += `\u{1F3C1}`
             }
-
-        */
+          }*/            
+        
     }
-    
-    
-    /*
-    if(passo == 0){//no caso do passo ser indicado como ser será considerado 1
-        passo = 1
-        alert('O passo Indicado 0! Será Considerado 1.')
-    }
-    */
-
-   /* 
-   if (inicio < fim){
-        while (inicio <= fim){                      
-            result.innerHTML += `${inicio}, `
-            inicio = inicio + passo  
-        }
-    }else{
-        while(fim <= inicio){           
-            result.innerHTML += `${inicio}, `
-            inicio = inicio - passo
-        }
-    }
-    */
-
 }
