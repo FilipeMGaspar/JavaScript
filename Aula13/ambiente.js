acf4ea50d66c0bba50cd000093e7412b7cff4d6a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 12:04:58 +0100

    Aula13 - do{ }While - Teste lógico no fim

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,25 +1,25 @@
-var contador = 1;
+var contador = 1
 do{//Estrutura de Repetição com teste lógico no FIM
     //console.log('Tudo bem!');
-    console.log(`Passo ${contador}`);
-    contador ++;//contador = contador+1
-}while(contador<=6);
+    console.log(`Passo ${contador}`)
+    contador ++//contador = contador+1
+}while(contador<=6)
 
 /*
-var contador = 1;
+var contador = 1
 while(contador <= 6){//Estrutura de repetição com teste lógico no inicio
-    //console.log('Tudo bem!');
-    console.log(`Passo ${contador}`);
+    //console.log('Tudo bem!')
+    console.log(`Passo ${contador}`)
     contador ++;//contador = contador+1
 }
 */
 
 /*
-console.log('Tudo bem!');
-console.log('Tudo bem!');
-console.log('Tudo bem!');
-console.log('Tudo bem!');
-console.log('Tudo bem!');
-console.log('Tudo bem!');
-console.log('Tudo bem!');
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
 */
