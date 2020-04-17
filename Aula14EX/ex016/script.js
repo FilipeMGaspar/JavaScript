2db34acf9fcc38d59864fb9e93e28afa9abc7386
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-17 12:18:11 +0100

    Exercicio - Aula14 - Ciclos de Repetição

--- Aula14EX/ex016/script.js
+++ Aula14EX/ex016/script.js
@@ -1,15 +1,22 @@
 function contar(){
     //txtini
     var txtini = document.getElementById('txtini')//recebe o valor da caixa de texto Inicio do HTML
+    var inicio = Number(txtini.value)//conversão para valor numérico 
        
     //txtfim
     var txtfim = document.getElementById('txtfim')//recebe o valor da caixa de texto fim do HTML 
+    var fim = Number(txtfim.value)//conversão para valor numérico 
     
     //txtpasso
     var txtpasso = document.getElementById('txtpasso')//recebe o valor da caixa de texto passo do HTML
+    var passo = Number(txtpasso.value)//conversão para valor numérico 
    
     //result
     var result = document.getElementById('result')//recebe o elemento <div< com id result
 
-    
+    while (inicio < fim){
+        passo+= inicio;
+        result.innerHTML= `teste${passo}`
+    }
+
 }
