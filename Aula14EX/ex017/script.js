a15c2fff777008c5c9aaeb2e2736fa8adce85115
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-18 13:16:58 +0100

    Exercicio02 - Aula14 - Tabuada Ciclo for

--- Aula14EX/ex017/script.js
+++ Aula14EX/ex017/script.js
@@ -9,17 +9,41 @@ function tabuada(){
         num = Number(num.value)
         let cont=1
         seletab.innerHTML = ''//limpa a área da tabuada
-        while(cont<=10){
-            let iten = document.createElement('option')//document.createElement('option') cria o elemento (<option></option>)
-            iten.text=`${num} x ${cont} = ${num*cont}`
-            iten.value=`tab${cont}`
-            seletab.appendChild(iten)//cria o objeto  (<option>  1x1=1 </option>) e sucessivamente
-            cont++
-        }
-    }
-   /* for(let cont = 1; cont=10; cont++ ){
+        
+        //utilizando o Ciclo While
 
-    }*/
-    //seletab.setAttribute('selecte' )
+        /*while(cont<=10){
+            
+            let iten = document.createElement('option')//document.createElement('option') cria o elemento (<option></option>)
+            
+            iten.text=`${num} x ${cont} = ${num*cont}`//cria o texto a apresentar pelo option
+            
+            iten.value=`tab${cont}`//cria o valor para o envio para o servidor no caso do PHP
+
+            seletab.appendChild(iten)//cria o objeto dentro do <select> <option>  1x1=1 </option> </select> e sucessivamente
+            cont++
+        }*/
+
+        //utilização do ciclo For
+        for(let conta=1; conta<=10;conta++){
+            //vamos criar o elemento option <option></option>
+            let iten = document.createElement('option')
+            
+            //vamos escrever dentro do <option> iten.text </option>
+            iten.text=`${num} x ${conta} = ${num*conta} `
+
+            //vamos atribuir o elemento value ao option <option value="iten.value"> iten.text </option>
+            iten.value=`tab${conta}`
+
+            /*
+                vamos adicionar os elementos dentro da tag: 
+                <select id="seltab">
+                    <option value="iten.value"> iten.text </option>
+                </select>
+            */
+           seletab.appendChild(iten)
+        }
+
+    }  
 
 }
