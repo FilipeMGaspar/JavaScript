1995ecba1cd29a7aaf7f8d9df1a17ff6496824de
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-18 12:50:05 +0100

    Exercicio02 - Aula14 - Tabuada

--- Aula14EX/ex017/script.js
+++ Aula14EX/ex017/script.js
@@ -1,16 +1,18 @@
 function tabuada(){
-    alert('Vamos á tabuada')
+   
     let num = document.getElementById('txtnum')
     let seletab = document.getElementById('seltab')
 
-    if (num.value.length== 0){
+    if (num.value.length == 0){
         alert('[ERRO] Está em falta o Número! Impossivel Calcular Tabuada')
     }else{
-        let ntab = Number(num.value)
-        for(let cont=1; cont<=10;cont++){
-            let mult = cont*ntab
-            seletab.appendChild('option')
-            seletab.setAttribute('value',`${cont} x ${ntab} = ${mult}` )
+        num = Number(num.value)
+        let cont=1
+        while(cont<=10){
+            let iten = document.createElement('option')//document.createElement('option') cria o elemento option
+            iten.text=`${num} x ${cont} = ${num*cont}`
+            seletab.appendChild(iten)//cria o objeto dentro da div
+            cont++
         }
     }
    /* for(let cont = 1; cont=10; cont++ ){
