07726affb82bed0005bf49f1233f32260a0c4ce8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 13:57:12 +0100

    Aula15 - Array - length

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,3 +1,14 @@
 let num = [5,8,4]
-console.log(`O nosso vetor é o {${num}} `)
-num.push()//adiciona o elemento automáticamente ao Vetos
+console.log(`O nosso vetor é o ${num} `)
+console.logo(`O vetor tem ${num.length} elementos`)
+
+
+/* ????? -> APONTAMENTOS <- ??????
+
+num.push(7)//adiciona o elemento automáticamente ao Vetor crienado a indice
+
+num.length// length conta o número de elementos no array
+
+num.some()//coloca todos os elementos por ordem crecente
+*/
+
