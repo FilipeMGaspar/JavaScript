359cf2a10de479d6772c56ba287e30f93e45843a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 16:00:46 +0100

    Aula16 - Função Recursiva introdução

---
+++ Aula16/funcao05.js
@@ -0,0 +1,16 @@
+//recursividade
+
+function factorial(num){
+    if(num==1){
+        return 1
+    }else{
+        return n * factorial(num-1)
+    }
+}
+
+//Factorial explicação
+
+// 5! = 5 *4 *3 *2 *1
+//5! = 5 * 4!
+
+// 1! 0 1
