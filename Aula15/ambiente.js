dd6606ff6bf34a00d8a4bda1c6dea2dd43a1e6ce
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 14:02:08 +0100

    Aula15 - Array - sort() - Ordem crecente

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,6 +1,10 @@
-let num = [5,8,4]
+let num = [5,8,2,9,3]
+num.push(1)//Adiciona o 1 ao vetor na ultima posição
+num.sort()//coloca os elementos por ordem crecente
 console.log(`O nosso vetor é o ${num} `)
-console.logo(`O vetor tem ${num.length} elementos`)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro elemento do vetor é: ${num[0]}`)
+
 
 
 /* ????? -> APONTAMENTOS <- ??????
