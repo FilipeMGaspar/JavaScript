34121488aa744636e3ef200ad3652092a04d6b25
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 16:20:50 +0100

    Exercicio - Aula16 - com Array e function

---
+++ Aula16ex/ex018/script.js
@@ -0,0 +1,20 @@
+/*
+Adicionar Números entre 1 e 100 validando 
+
+o botão Adicionar adiciona o valor a um array
+
+Apresentando num <select> <option></option> </select>
+
+botão finalizar:
+    apresenta o total de valores no Array
+    Mostra o maior e o menor valor 
+    a Soma de  todo os valores
+    e a média dos valores no array
+
+Adicionando novo valor válido através do botão Adicionar    limpa a informação apresentada na segunda div
+
+No caso de a caixa de texto onde o número é adicionado estar vazia vai ser emitido um erro
+
+O programa verifica se tentar finalizar sem dados introduzidos emite o Alerta de ERRO
+
+*/
