340111a9af601437d4921ba7c20b15e34e60f97f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 14:12:24 +0100

    Aula15 - Array - num.push() // adiciona elemento fim do vetor

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,6 +1,10 @@
 let num = [5,8,2,9,3]
-num.push(1)//Adiciona o 1 ao vetor na ultima posição do ARRAY
+num.push(4)//Adiciona o 1 ao vetor na ultima posição do ARRAY
+
 num.sort()//coloca os elementos por ordem crecente
+
+num.push(1)//Adiciona o 1 ao vetor na ultima posição do ARRAY
+
 console.log(`O nosso vetor é o ${num} `)
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro elemento do vetor é: ${num[0]}`)
