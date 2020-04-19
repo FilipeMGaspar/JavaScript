1de785a72c0de96c3e64e9aea8f44784ee335cb7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-19 14:18:58 +0100

    Aula15 - Array - utilizando o ciclo FOR

--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -1,8 +1,11 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
-// console.log(valores)
+console.log(valores)
 
 /*
+
+Forma incorrecta
+
 console.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
@@ -12,5 +15,5 @@ console.log(valores[5])
 */
 
 for(let pos=0;pos<=valores.length;pos++){
-    console.log(`${valores[pos]}`)
+    console.log(`A possição ${pos} tem o valor ${valores[pos]}`)
 }
