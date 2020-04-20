bc68db5ad06ffcfe9b2bc6714c3f17c935beaeca
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-20 19:06:39 +0100

    Exercicio - Aula16 - JS - Validação entre 1 e 100 emojis

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -26,13 +26,13 @@ function adic(){
     let res = document.getElementById('result')
 
     if(num.value.length == 0){
-        alert('[ERRO] Nenhum número detectado. Impossivel Adicionar!')
+        alert(`\u{2639} [ERRO] Nenhum número detectado. Impossivel Adicionar! `)
     }else{
         let nr = Number(num.value)
         if(nr>=1 && nr<=100){
             
         }else{
-            alert('[!! ERRO !!] Número Inválido. Insira entre 1 e 100')
+            alert(`\u{2639} [ERRO] Número Inválido. Insira entre 1 e 100`)
         }
 
     }
@@ -42,6 +42,6 @@ function fim(){
     let selnum =document.getElementById('selnum')
 
     if(selnum.value.length==0){
-        alert("[ERRO] Nenhum Valor Detectado. Impossivel Finalizar")
+        alert("\u{2639} [ERRO] Nenhum Valor Detectado. Impossivel Finalizar")
     }
 }
