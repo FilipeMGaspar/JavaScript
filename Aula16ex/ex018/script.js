16c8d8a2f203a6afb9fcbfe009fd5d546d60b2ff
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-21 17:42:17 +0100

    Exercicio - Aula16 - Finalizado
    A Minha solução para o problema

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -1,11 +1,11 @@
-
+//Zona de variaveis globais
 let val=[] // criaçaõ de variavel global do tipo array.
 let divres = document.getElementById('result')
+let num = document.getElementById('txtnum')
 
-
+//função que verifica se a caixa de texto extá vazia  e que chama a função que adiciona os valores ao vetor e os apresenta na tela
 function adic(){
-    let num = document.getElementById('txtnum')
-
+   
     if(num.value.length == 0){
         alert(`\u{2639} [ERRO] Nenhum número detectado. Impossivel Adicionar! `)
     }else{
@@ -14,7 +14,6 @@ function adic(){
             if(nr>=1 && nr<=100){  
                                  
                 adiciona(nr)//chamada da função adiciona os valores ao Vetor e os itens ao select
-                num.value = ''//limpa a caixa do número
 
             }else{
                 alert(`\u{2639} [ERRO] Número Inválido. Insira entre 1 e 100`)
@@ -22,30 +21,41 @@ function adic(){
     }
 }
 
+
+//função que verifica se o vetor (Array) já contem algum valor e no caso de já não estar vazio chama a função de calculo
 function fim(){
 
     if(val.length==0){
-        alert("\u{2639} [ERRO] Nenhum Valor Detectado. Impossivel Finalizar")
+        alert(`\u{2639} [ERRO] Nenhum Valor Detectado. Impossivel Finalizar`)
     }else{
         calcula()//chamada para a função que calcula a soma a média o maior e o menor, imprimindo na tela
     }
 }
 
+
 //função para adicionar valores ao array e adicionar elementos ao select
 function adiciona(numer){
     let selnum =document.getElementById('selnum')
 
+    let repet = val.indexOf(numer)//Verifica se o elemento se encontar no ARRAY
+   
     divres.innerHTML = ''//Limpa a div onde surgem os resultados
 
-    val.push(numer)    
-    let item = document.createElement('option')
-    item.text = `Valor ${numer} foi adicionado`    
-    selnum.appendChild(item)     
+    if(repet == -1){       
+        val.push(numer)    
+        let item = document.createElement('option')
+        item.text = `Valor ${numer} foi adicionado`    
+        selnum.appendChild(item)
+        num.value = ''//limpa o valor contido na caixa de texto
+    }else{
+        alert(`\u{2639} [ERRO] O Número ${numer} já foi inserido`)
+    }
 }
+
 
 //Função para calcular a soma, a média, o maior e o menor dos elementos do vetor e o número de elementos inseridos no array
 function calcula(){
-    let nrelem = val.length;
+    let nrelem = val.length
     
     if(nrelem== 1){
         divres.innerHTML = `<p> No total registámos 
