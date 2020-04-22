91764b9b0aedf2a27b43ba0a2a5029a7ee5698be
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-22 10:44:57 +0100

    Exercicio - Aula16 - Solução do CEV

--- Aula16ex_CEV/ex018Prof/script.js
+++ Aula16ex_CEV/ex018Prof/script.js
@@ -31,3 +31,14 @@ function adicionar(){
     num.value = ''
     num.focus()
 }
+
+function finalizar(){
+    if (valores.length == 0){
+        alert('Adicionar valores antes de finalizar')
+    }else{
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+    }
+}
