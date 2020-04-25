9a803ad0e653b01370f39e05a80347143a644613
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-25 17:03:08 +0100

    Educativo 4º Ano 1º Ciclo

--- Educativo_4_Ano/script.js
+++ Educativo_4_Ano/script.js
@@ -1,7 +1,8 @@
 //let conteudo = document.getElementById('conteudo')
 let conteudo = document.querySelector('div#conteudo')
 let questao = document.querySelector('div#questao')
-
+let resp1 = document.querySelector('div#resp1')
+let resp2 = document.querySelector('div#resp2')
 function carregar() {
     conteudo.innerHTML=''
     conteudo.innerHTML+= `<p>Poluição Aquática!</p>`
@@ -10,5 +11,7 @@ function carregar() {
     imagem.setAttribute('src', 'imagens/Pulagua.png')
     conteudo.appendChild(imagem)
     questao.innerHTML=`Quais são os principais poluentes da água? `
+    resp1.innerHTML = '<p>Esgoto, Petroleo, Metais-pesados, Pesticidas, Óleo de cozinha, Etc...<p>'
+    resp2.innerHTML = '<p>Esgoto, Resíduos Sólidos, Estações de Tratamento, Reciclagem, Rega Etc...<p>'
 
 }
