a18be73f877be905af9d79da3a276ecd99e35b52
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-25 17:17:13 +0100

    Educativo 4ºAno 1ºCiclo

--- Educativo_4_Ano/script.js
+++ Educativo_4_Ano/script.js
@@ -3,6 +3,8 @@ let conteudo = document.querySelector('div#conteudo')
 let questao = document.querySelector('div#questao')
 let resp1 = document.querySelector('div#resp1')
 let resp2 = document.querySelector('div#resp2')
+let resp3 = document.querySelector('div#resp3')
+let resp4 = document.querySelector('div#resp4')
 function carregar() {
     conteudo.innerHTML=''
     conteudo.innerHTML+= `<p>Poluição Aquática!</p>`
@@ -12,6 +14,7 @@ function carregar() {
     conteudo.appendChild(imagem)
     questao.innerHTML=`Quais são os principais poluentes da água? `
     resp1.innerHTML = '<p>Esgoto, Petroleo, Metais-pesados, Pesticidas, Óleo de cozinha, Etc...<p>'
-    resp2.innerHTML = '<p>Esgoto, Resíduos Sólidos, Estações de Tratamento, Reciclagem, Rega Etc...<p>'
-
+    resp2.innerHTML = '<p>Esgoto, resíduos sólidos, estações de tratamento, reciclagem Etc...<p>'
+    resp3.innerHTML = '<p>Fumo, barulho excessivo, um Livro, Estações de Tratamento, Etc...<p>'
+    resp4.innerHTML=`<p>Beber água, fazer limonada, fechar a torneira </p>`
 }
