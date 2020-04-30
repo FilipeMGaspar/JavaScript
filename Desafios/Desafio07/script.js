213ed054f1efe5f8ba69cf783f8811c5f191e20b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-30 19:43:26 +0100

    Desafio07

--- Desafios/Desafio07/script.js
+++ Desafios/Desafio07/script.js
@@ -1,16 +1,34 @@
 
 let valores = []
+let num = document.getElementById('txtnum')
 
 function inserir(){
-    let num = document.getElementById('txtnum') 
+     
     if(num.value.length == 0){
         alert('[ERRO] Nenhum Número detetado!')
     }else{
-        let selnum = document.querySelector('select#selnum')
-        let item = document.createElement('option')
-        item.text = `Recebi: ${Number(num.value)}`
-        selnum.appendChild(item)
-        num.value = ''
-        num.focus()
+        verNaLista(Number(num.value), valores)
+    }
+}
+
+function adicionar(nr){
+
+    let selnum = document.querySelector('select#selnum')
+    let item = document.createElement('option')
+    valores.push(nr)
+    item.text = `Recebi: ${nr}`
+    selnum.appendChild(item)
+    num.value = ''
+    num.focus()
+}
+
+
+function verNaLista(n, l){
+
+    let existe = l.indexOf(n)
+    if(existe == -1){
+        adicionar(n)
+    }else{
+        alert(`O Número ${n} já se encontra na lista`)
     }
 }
