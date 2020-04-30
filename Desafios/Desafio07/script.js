912e9fdb3cb62b651bf5586a5ac66ed308ff3184
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-30 19:24:24 +0100

    Desafio07

--- Desafios/Desafio07/script.js
+++ Desafios/Desafio07/script.js
@@ -1,10 +1,16 @@
 
+let valores = []
 
 function inserir(){
     let num = document.getElementById('txtnum') 
     if(num.value.length == 0){
         alert('[ERRO] Nenhum Número detetado!')
     }else{
-        
+        let selnum = document.querySelector('select#selnum')
+        let item = document.createElement('option')
+        item.text = `Recebi: ${Number(num.value)}`
+        selnum.appendChild(item)
+        num.value = ''
+        num.focus()
     }
 }
