193ce64f73a0479b2f9e4c389ad5900d5865f176
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-04-30 19:05:33 +0100

    Desafio07

---
+++ Desafios/Desafio07/script.js
@@ -0,0 +1,11 @@
+let num = document.getElementById('txtnum')
+
+
+function inserir(){
+    if(num.value.lenght == 0){
+        alert(`[ERRO] Nenhum valor Adicionado`)
+    }else{
+        alert('Tudo OK')
+    }
+    
+}
