8acd3925d00bc4a496eb3689c1c9b6e27d4c5264
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-03 17:58:09 +0100

    Desafio08

--- Desafios/Desafio08/script.js
+++ Desafios/Desafio08/script.js
@@ -3,15 +3,22 @@ let numtestes = document.querySelector('div#notas')
 function registanotas(){
     let   NrTestes = document.getElementById('txtNrTestes')
     if(NrTestes.value.length == 0){
-       alert('[ERRO] Não foi indicado o Número de Testes')
+       alert('[ERRO] Número de Testes. Não preenchido!!!')
         NrTestes.focus()
     }else{
         if(Number(NrTestes.value)<0){
             alert(`[ERRO] O Número de Testes Indicado ${Number(NrTestes.value)} não é correcto.`)
             NrTestes.focus()
             NrTestes.value = ''
-        }else{   
-            alert (`A sua turma fez ${Number(NrTestes.value) } Testes`)
+        }else{ 
+            mostraNaTela(Number(NrTestes.value))
         }
     }
+}
+
+function mostraNaTela(ntestes){
+    numtestes.innerHTML = ''
+    numtestes.innerHTML += `<h1>Gestor de Notas</h1>`
+    numtestes.innerHTML += `<P>A sua turma fez ${ntestes} Testes.</p>`  
+    alert (`A sua turma fez ${ntestes} Testes`)
 }
