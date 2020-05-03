50dd9a725bf9f067de87107b3ab69b1e161dea26
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-03 17:49:49 +0100

    Desafio08

--- Desafios/Desafio08/script.js
+++ Desafios/Desafio08/script.js
@@ -1,11 +1,17 @@
 let numtestes = document.querySelector('div#notas')
 
 function registanotas(){
-    alert('Iniciei o REgisto de notas')
-    let NrTestes= document.querySelector('input#txtNrTestes')
-    if(NrTestes.value.lenght == 0){
-       alert('[ERRO] Impossivel iniciar')
+    let   NrTestes = document.getElementById('txtNrTestes')
+    if(NrTestes.value.length == 0){
+       alert('[ERRO] Não foi indicado o Número de Testes')
+        NrTestes.focus()
     }else{
-        alert (`A sua turma fez ${Number(NrTestes.value) } Testes`)
+        if(Number(NrTestes.value)<0){
+            alert(`[ERRO] O Número de Testes Indicado ${Number(NrTestes.value)} não é correcto.`)
+            NrTestes.focus()
+            NrTestes.value = ''
+        }else{   
+            alert (`A sua turma fez ${Number(NrTestes.value) } Testes`)
+        }
     }
 }
