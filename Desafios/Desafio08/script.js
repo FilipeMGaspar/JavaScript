697a0ad91945f2456d8f1628f145a86938eccd1f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-03 17:01:43 +0100

    Desafio08

---
+++ Desafios/Desafio08/script.js
@@ -0,0 +1,8 @@
+let numtestes = document.querySelector('div#notas')
+
+function registanotas(){
+    alert('Iniciei o REgisto de notas')
+    let NrTestes= document.querySelector('input#txtNrTestes')
+    
+    alert (`A sua turma fez ${Number(NrTestes.value)} Testes`)
+}
