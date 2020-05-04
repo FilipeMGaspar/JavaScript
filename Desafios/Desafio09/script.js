ba1d56275a0b1dbe3eac30e7a4c471207b746d7f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-04 16:46:03 +0100

    Desafio09

--- Desafios/Desafio09/script.js
+++ Desafios/Desafio09/script.js
@@ -1,3 +1,21 @@
+let txtnum = document.querySelector('input#txtnum')
+let selnum = document.querySelector('select#selnum')
+
 function inserir(){
-    alert ('Bem vindo!')
+    
+    if (txtnum.value.length == 0){
+        alert('!! [ERRO] !! nenhum número informado.')
+        txtnum.focus()
+    }else{
+        adicionarNaLista(Number(txtnum.value))
+    }
+}
+
+function adicionarNaLista(num){
+
+    let itemnr = document.createElement('option')
+    itemnr.text = `Adicionei o Nº: ${num}`
+    selnum.appendChild(itemnr)
+    
+    //alert(`Número ${num}`)
 }
