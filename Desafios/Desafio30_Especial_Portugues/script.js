24faacff4254a8e92fc3b0f5855f65516a6a34e6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-06 18:58:45 +0100

    Desafio30-Especial de Português (a != há))

--- Desafios/Desafio30_Especial_Portugues/script.js
+++ Desafios/Desafio30_Especial_Portugues/script.js
@@ -1,9 +1,15 @@
 let resp= [ 'a', 'há']
-let selresp = document.querySelector('select#selresp')
-let valselresp = selresp.options[selresp.selectedIndex]
+
 
 function verificar(){
+    let selresp = document.querySelector('select#selresp')
+    let valselresp = selresp.options[selresp.selectedIndex]
+    
+    let selresp2 = document.querySelector('select#selresp2')
+    let valresp2 = selresp2.options[selresp2.selectedIndex]
+
     alert('Entrei aki')
-    alert (`VAmos lá verificar ${valselresp.text}`)
+    alert (`Primeira parte ${valselresp.text}`)
+    alert(`Segunda Parte ${valresp2.text}`)
 
 }
