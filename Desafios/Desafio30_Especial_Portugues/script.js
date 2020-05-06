e28524dafb2d79dc40a90c964a401f109bde4167
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-06 19:16:50 +0100

    Desafio30-Especial de Português (a != há))

--- Desafios/Desafio30_Especial_Portugues/script.js
+++ Desafios/Desafio30_Especial_Portugues/script.js
@@ -7,12 +7,29 @@ function verificar(){
     let selresp2 = document.querySelector('select#selresp2')
     let valselresp2 = selresp2.options[selresp2.selectedIndex]
 
-    if(valselresp == 'a' && valselresp2 =='há'){
+    let selresp3 = document.querySelector('select#selresp3')
+    let valselresp3 = selresp3.options[selresp2.selectedIndex]
+
+    
+
+   
+    let respondido = verrespostas(valselresp.text, valselresp2.text, valselresp3.text)
+
+    if(respondido){
         alert(`Parabéns!!!`)
     }else{
+        valselresp.text = 'a'   
         valselresp2.text = 'a'
-        valselresp.text = 'a'        
+        valselresp3.text = 'Há'     
         alert('!! [ERRO] !! Verifica de novo')
     }
 
+}
+
+function verrespostas(r1, r2, r3){
+    if(r1 == 'a' && r2 == 'a' && r3 == 'Há' ){
+        return true
+    }else{
+        return false
+    }
 }
