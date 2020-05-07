19ecb82df8336dd176eeb277ea4283561142bd32
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-07 11:48:54 +0100

    Desafio30-Especial de Português (a != há))

--- Desafios/Desafio30_Especial_Portugues/script.js
+++ Desafios/Desafio30_Especial_Portugues/script.js
@@ -42,8 +42,11 @@ function verificar(){
 
     let selresp14 = document.querySelector('select#selresp14')
     let valselresp14 = selresp14.options[selresp14.selectedIndex]
+
+    let selresp15 = document.querySelector('select#selresp15')
+    let valselresp15 = selresp15.options[selresp15.selectedIndex]
    
-    let respondido = verrespostas(valselresp.text, valselresp2.text, valselresp3.text, valselresp4.text, valselresp5.text, valselresp6.text , valselresp7.text, valselresp8.text, valselresp9.text, valselresp10.text, valselresp11.text, valselresp12.text, valselresp13.text, valselresp14.text)
+    let respondido = verrespostas(valselresp.text, valselresp2.text, valselresp3.text, valselresp4.text, valselresp5.text, valselresp6.text , valselresp7.text, valselresp8.text, valselresp9.text, valselresp10.text, valselresp11.text, valselresp12.text, valselresp13.text, valselresp14.text, valselresp15.text)
 
     if(respondido){
         alert(`Parabéns!!! Acertas-te em todas \u{1F603}`)
@@ -63,14 +66,15 @@ function verificar(){
         valselresp12.text = 'Há'
         valselresp13.text = 'há'
         valselresp14.text = 'A'
+        valselresp15.text = 'a'
         alert('!! [ERRO] !! Verifica de novo')
     }
 
 }
 
-function verrespostas(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14){
+function verrespostas(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15){
      
-    if(r1 === 'a' && r2 === 'a' && r3 === 'Há' && r4 === 'há' && r5 === 'a' && r6 === 'há' && r7 === 'há'  && r8 === 'há'   && r9 === 'há' && r10 === 'A' && r11 === 'há' && r12 === 'Há' && r13 === 'há' && r14 === 'A'){
+    if(r1 === 'a' && r2 === 'a' && r3 === 'Há' && r4 === 'há' && r5 === 'a' && r6 === 'há' && r7 === 'há'  && r8 === 'há'   && r9 === 'há' && r10 === 'A' && r11 === 'há' && r12 === 'Há' && r13 === 'há' && r14 === 'A' && r15 === 'a'){
         return true
     }else{
         return false
