12bc38922ef41b37681d21318fb8766e7373a62f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-07 11:18:22 +0100

    Desafio30-Especial de Português (a != há))

--- Desafios/Desafio30_Especial_Portugues/script.js
+++ Desafios/Desafio30_Especial_Portugues/script.js
@@ -33,8 +33,11 @@ function verificar(){
 
     let selresp11 = document.querySelector('select#selresp11')
     let valselresp11 = selresp11.options[selresp11.selectedIndex]
+
+    let selresp12 = document.querySelector('select#selresp12')
+    let valselresp12 = selresp12.options[selresp12.selectedIndex]
    
-    let respondido = verrespostas(valselresp.text, valselresp2.text, valselresp3.text, valselresp4.text, valselresp5.text, valselresp6.text , valselresp7.text, valselresp8.text, valselresp9.text, valselresp10.text, valselresp11.text)
+    let respondido = verrespostas(valselresp.text, valselresp2.text, valselresp3.text, valselresp4.text, valselresp5.text, valselresp6.text , valselresp7.text, valselresp8.text, valselresp9.text, valselresp10.text, valselresp11.text, valselresp12.text)
 
     if(respondido){
         alert(`Parabéns!!!`)
@@ -51,14 +54,15 @@ function verificar(){
         valselresp9.text = 'há'
         valselresp10.text = 'A'
         valselresp11.text = 'há'
+        valselresp11.text = 'Há'
         alert('!! [ERRO] !! Verifica de novo')
     }
 
 }
 
-function verrespostas(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11){
+function verrespostas(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12){
      
-    if(r1 === 'a' && r2 === 'a' && r3 === 'Há' && r4 === 'há' && r5 === 'a' && r6 === 'há' && r7 === 'há'  && r8 === 'há'   && r9 === 'há' && r10 === 'A' && r11 === 'há' ){
+    if(r1 === 'a' && r2 === 'a' && r3 === 'Há' && r4 === 'há' && r5 === 'a' && r6 === 'há' && r7 === 'há'  && r8 === 'há'   && r9 === 'há' && r10 === 'A' && r11 === 'há' && r12 === 'Há'){
         return true
     }else{
         return false
