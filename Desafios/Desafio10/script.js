91af59007846065ccfb3122002a7b6d8ba73c123
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-10 12:27:46 +0100

    Desafio10 - Sequência Fibonacci

--- Desafios/Desafio10/script.js
+++ Desafios/Desafio10/script.js
@@ -8,32 +8,37 @@ function mostrar(){
         txtnum.focus()
     }else{
         let termo1 = 0
-        let termo2 =1
+        let termo2 = 1
 
         if(Number(txtnum.value)<= 0){
             alert('!! [ERRO] !! O Número de Termos inserido não é válido!')
             txtnum.focus()
             txtnum.value = ''
         }else if(Number(txtnum.value) == 1){
-            divresultado.innerHTML = `${termo1} - Fim`
+            divresultado.innerHTML =`<p>Solicitou mostrar ${Number(txtnum.value)} termo da Sequência Fibonacci!</p>`
+            divresultado.innerHTML += `${termo1} - Fim`
             txtnum.focus()
+            divresultado.innerHTML +=`<p>A soma de ${Number(txtnum.value)} termo é : 0</p>`
             txtnum.value = ''
-        }else{            
-            divresultado.innerHTML = `${termo1} - ${termo2} - `
+        }else{  
+            divresultado.innerHTML =`<p>Solicitou mostrar ${Number(txtnum.value)} termos da Sequência Fibonacci!</p>`
+            divresultado.innerHTML += `${termo1} - ${termo2} - `
         /*
                       termo3 = termo1+ termo2                 
         0         1       1
         termo1 - termo2 - termo3
         */
-            for(let conta = 3; conta<= Number(txtnum.value); conta++ ){
+            for(let conta = 2; conta<= Number(txtnum.value); conta++ ){
                 let termo3 = termo1 + termo2
                 divresultado.innerHTML += ` ${termo3} -`
                 termo1 = termo2
                 termo2 = termo3
             }
+            divresultado.innerHTML += ' Fim'
             txtnum.focus()
-            txtnum.value = ''
+            divresultado.innerHTML +=`<p>A soma dos ${Number(txtnum.value)} termos é : </p>`
+            txtnum.value = ''           
         }
-
+        
     }
 }
