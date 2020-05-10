29374d22034bf983e21349e8936beb59bcb60861
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-10 11:23:34 +0100

    Desafio10 - Sequência Fibonacci

---
+++ Desafios/Desafio10/script.js
@@ -0,0 +1,8 @@
+
+function mostrar(){
+    let txtnum = document.querySelector('input#txtnum')
+    let divresultado= document.querySelector('div#resultado')
+    let termo1 = 0
+    let termo2 =1
+    divresultado.innerHTML = `${termo1} - ${termo2}`
+}
