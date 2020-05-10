f7a0184547d7188b8c9c07318d097ed909d44e6a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-10 11:40:53 +0100

    Desafio10 - Sequência Fibonacci

--- Desafios/Desafio10/script.js
+++ Desafios/Desafio10/script.js
@@ -2,7 +2,27 @@
 function mostrar(){
     let txtnum = document.querySelector('input#txtnum')
     let divresultado= document.querySelector('div#resultado')
-    let termo1 = 0
-    let termo2 =1
-    divresultado.innerHTML = `${termo1} - ${termo2}`
+
+    if(txtnum.value.length == 0){
+        alert('!! [ERRO] !! O número de termos amostrar não foi indicado!')
+        txtnum.focus()
+    }else{
+        let termo1 = 0
+        let termo2 =1
+
+        
+        divresultado.innerHTML = `${termo1} - ${termo2} - `
+        /*
+                      termo3 = termo1+ termo2                 
+        0         1       1
+        termo1 - termo2 - termo3
+        */
+        for(let conta = 3; conta<= Number(txtnum.value); conta++ ){
+            let termo3 = termo1 + termo2
+            divresultado.innerHTML += ` ${termo3} -`
+            termo1 = termo2
+            termo2 = termo3
+        }
+
+    }
 }
