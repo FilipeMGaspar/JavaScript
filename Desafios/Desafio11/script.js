9bc8f97a17ce51acb5b8601a019c49dadf3ed3e9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-12 12:29:51 +0100

    Desafio11 - Calculadora de IMC

--- Desafios/Desafio11/script.js
+++ Desafios/Desafio11/script.js
@@ -7,7 +7,7 @@ function iniciar(){
 
 
 function calcular(){
-    if(txtpeso.value.length == 0){
+    if(txtpeso.value.length==0){
       alert('!! [ERRO] !! Valor do Peso não foi informado!')
       txtpeso.focus()
     }else if(txtaltura.value.length == 0){
@@ -15,9 +15,13 @@ function calcular(){
         txtaltura.focus()
     }else{
         if(Number(txtpeso.value)<=0){
-            alert(`!! [ERRO] !! Valor ${Number(txtpeso.value)} para o peso é incorreto!`)
+            alert(`!! [ERRO] !! valor: ${Number(txtpeso.value)} para o Peso é incorreto!`)
             txtpeso.focus()
             txtpeso.value = ''
+        }else if (Number(txtaltura.value)<=0 || Number(txtaltura.value)>=3){
+            alert(`!! [ERRO] !! O valor: ${Number(txtaltura.value)} para a Altura é inválido!`)
+            txtaltura.focus()
+            txtaltura.value = ''
         }else{
            alert('tudo ok podemos continuar')
         }
