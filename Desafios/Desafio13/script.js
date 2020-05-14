98c514c46906a37cc5f7e9e81125ead4979456ca
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 11:23:18 +0100

    Desafio13 - Multiplos de 3 e de 5

--- Desafios/Desafio13/script.js
+++ Desafios/Desafio13/script.js
@@ -2,3 +2,20 @@ let txtnum = document.querySelector('input#txtnum')
 let selnum = document.querySelector('select#selnum')
 let multiplos = []
 
+function iniciar(){
+    txtnum.focus()
+}
+
+function inserir(){
+   if(txtnum.value.length == 0){
+       alert('!! [ERRO] !! Nenhum Número informado!')
+       txtnum.focus()
+       txtnum.value = ''
+   }else{
+     colocarNaLista(Number(txtnum.value))
+   }
+}
+
+function colocarNaLista(numero){
+    
+}
