32243366397c6b4998d23490eb8fcd04f8f96fcb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-15 19:51:45 +0100

    Desafio14 - Maior ou menor de Idade >=18

--- Desafios/Desafio14/script.js
+++ Desafios/Desafio14/script.js
@@ -1,14 +1,16 @@
 let txtnum = document.querySelector('input#txtnum')
 let selectIdade = document.querySelector('select#selectIdade')
+let idade = []
 
 function iniciar(){
     txtnum.focus()
+    txtnum.value=''
 }
 
 function inserir(){
     if(txtnum.value.length==0){
         alert('!! [ERRO] !! Ano de Nascimento não foi informado!')
-        txtnum.focus()
+        iniciar()
     }else{
         colocaNalista(Number(txtnum.value))
     }
@@ -18,11 +20,25 @@ function colocaNalista(anoNascimento){
     let itemOption= document.createElement('option')
 
     if(anoNascimento>=0){
-        itemOption.text = `Inseriu o ano de: ${anoNascimento}`
-        selectIdade.appendChild(itemOption)
+        if (estaNaLista(anoNascimento, idade)){
+            itemOption.text = `Inseriu o ano: ${anoNascimento}`
+            selectIdade.appendChild(itemOption)
+            idade.push(anoNascimento)
+            iniciar()
+        }else{
+            alert(`O ano de ${anoNascimento} já se encontra na Lista!`)
+            iniciar()
+        }
     }else{
-        alert('')
+        alert(`O ano ${anoNascimento} é inválido`)
+        iniciar()
     }
+}
 
-   
+function estaNaLista(ano, LstIdade){
+    if (LstIdade.indexOf(ano)==-1){
+        return true
+    }else{
+        return false
+    }
 }
