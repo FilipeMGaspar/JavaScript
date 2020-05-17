fa4673b20582b7855a159850e82fe21b65188357
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-17 11:20:56 +0100

    Desafio15- Nomes em ordem invertida

--- Desafios/Desafio15/script.js
+++ Desafios/Desafio15/script.js
@@ -1,7 +1,25 @@
 let cNome  = document.querySelector('input#cNome')
+let selNome = document.querySelector('select#selNome')
 
 function iniciar(){
     cNome.focus()
     cNome.value=''
 }
 
+function inserir(){
+    if(cNome.value.length == 0){
+        alert('!! [ERRO] !! Caixa de nome Vazia!')
+        iniciar()
+    }else{
+        colocaNaLista(cNome.value)
+    }
+}
+
+function colocaNaLista(nome){
+    alert(`Nome recebido ${nome}`)
+    let itemOption = document.createElement('option')
+
+    itemOption.text = `-> ${nome}`
+    selNome.appendChild(itemOption)
+
+}
