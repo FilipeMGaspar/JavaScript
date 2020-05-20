f4f000781acd33396de144d24a474ce687b1904b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-20 12:39:41 +0100

    Exercicio09 - Adicional - Calculadora de juros

--- Desafios/Desafio24_adicional_09/script.js
+++ Desafios/Desafio24_adicional_09/script.js
@@ -9,8 +9,16 @@ function iniciar(){
 }
 
 function ativaCxJuros(){
-    cJuros.removeAttribute('readonly')
-    cJuros.focus()
+    if(Number(cValEmpres.value)>=250){
+        cJuros.removeAttribute('readonly')
+        cJuros.focus()
+    }else{
+        alert(`!! [ERRO] !! O Valor ${Number(cValEmpres.value)} não é válido para empréstimo. Valor minimo de 250€`)
+        cValEmpres.focus()
+        cValEmpres.value= '250'
+        cJuros.removeAttribute('readonly')
+        cJuros.focus()
+    }
 }
 
 function calcular(){
@@ -21,6 +29,16 @@ function calcular(){
     }else{
         if (cJuros.value.length == 0){
             alert('!! [ERRO] !! Valor de juros não indicado')
+            cJuros.focus()
+            cJuros.value= ''
+        }else{
+            if(Number(cJuros.value)>=2 && Number(cJuros.value)<=26){
+                alert('tudo ok podemos continuar')
+            }else{
+                alert(`!! [ERRO] !! O valor ${Number(cJuros.value)} não é válido indique entre 2% e 26%.`)
+                cJuros.focus()
+                cJuros.value= '2'
+            }
         }
     }
 }
