57524d6ad4a26d1ff2196b8ff44c2d578ed748a2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-20 12:09:06 +0100

    Exercicio09 - Adicional - Calculadora de juros

---
+++ Desafios/Desafio24_adicional_09/script.js
@@ -0,0 +1,14 @@
+let cJuros = document.querySelector('input#cJuros')
+
+let cValEmpres = document.querySelector('input#cValEmpres')
+
+
+function iniciar(){
+    cValEmpres.focus()
+    cValEmpres.value= ''
+}
+
+function ativaCxJuros(){
+    cJuros.removeAttribute('readonly')
+}
+
