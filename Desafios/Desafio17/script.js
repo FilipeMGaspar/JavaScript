58fc9e1e1e26a82e41aa50a01536e7cd71c5e536
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-30 14:55:20 +0100

    Desafio17 - Avaliador de Filmes

--- Desafios/Desafio17/script.js
+++ Desafios/Desafio17/script.js
@@ -1,4 +1,5 @@
 let CxNum = document.querySelector('input#CxNum')
+let cSelOpinao = document.querySelector('select#cSelOpinao')
 
 function iniciar(){
     CxNum.focus()
@@ -9,5 +10,26 @@ function Adicionar(){
     let cSelOpina = document.querySelector('select#cSelOpina')
     let valselresp = cSelOpina.options[cSelOpina.selectedIndex]
     
-    alert(valselresp.text)
+    if(CxNum.value.length==0){
+        alert("!! [ERRO] !! Idade não informada!")
+        iniciar()
+    }else{
+        if(verificaIdade(Number(CxNum.value))){
+            alert(valselresp.text)
+        }else{
+            alert(`!! [ERRO] !! a Idade de ${Number(CxNum.value)} Anos. É inválida!`)
+            iniciar()
+        }
+        
+    }
+}
+
+function verificaIdade(Idade){
+
+    if((Idade>0) && Idade<123){
+        return true
+    }else{
+        return false
+    }
+
 }
