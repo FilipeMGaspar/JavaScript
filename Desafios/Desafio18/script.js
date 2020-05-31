d7c7f993695cbe4055492018b8b7c201dcd68e84
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-31 19:58:11 +0100

    Desafio18 - Idade, Genero, Totalizador

--- Desafios/Desafio18/script.js
+++ Desafios/Desafio18/script.js
@@ -4,3 +4,13 @@ function inicar(){
     CxAnoNasc.focus()
     CxAnoNasc.value=''
 }
+
+function FcAdicionar(){
+    if(CxAnoNasc.value.length == 0){
+        alert('!!! [ERRO] !!! Ano de nascimento não foi informado!')
+        inicar()
+    }else{
+        alert('Tudo ok podemos continuar!')
+    }
+
+}
