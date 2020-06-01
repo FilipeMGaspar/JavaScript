4b9114b4789bcb521a554f0619d68f139fa83d81
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-01 10:18:43 +0100

    Desafio18 - Idade, Genero, Totalizador

--- Desafios/Desafio18/script.js
+++ Desafios/Desafio18/script.js
@@ -1,4 +1,5 @@
 let CxAnoNasc = document.querySelector('input#CxAnoNasc')
+let GenSelecionado = document.getElementsByName('Genero')
 anoAtual = new Date
 let estouNoAno = anoAtual.getFullYear()
 
@@ -13,7 +14,11 @@ function FcAdicionar(){
         inicar()
     }else{
         if (verificaAno(Number(CxAnoNasc.value))){
-            alert('Tudo ok podemos continuar!')
+            if(RadioGeneroSelecionado()){
+                alert('Tudo ok podemos continuar!')
+            }else{
+                alert('!! [ERRO] !! Género não foi indicado! Por favor Selecione (Feminino ou Masculino)')
+            }
         }else{
             alert(`!! [ERRO] !! O Ano de ${Number(CxAnoNasc.value)} não é válido! Voce teria ${estouNoAno - Number(CxAnoNasc.value)} Anos`)
         }
@@ -28,4 +33,14 @@ function verificaAno(AnoNascimento){
     }else{
         return false
     }
+}
+
+function RadioGeneroSelecionado(){
+    //let GenSelecionado = document.getElementsByName('Genero')
+    //let Genero = ''  
+    if ((GenSelecionado[0].checked==true) && (GenSelecionado[1].checked==true)){
+       return true
+   }else{
+        return false
+   }
 }
