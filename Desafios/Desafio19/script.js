de12e53fa6b897440e9259bc2e6176a32b602c19
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-04 17:48:37 +0100

    Desafio19 - Formula resolvente - equações quadráticas

---
+++ Desafios/Desafio19/script.js
@@ -0,0 +1,12 @@
+let CxA = document.querySelector('input#CxA')
+let CxB = document.querySelector('input#CxB')
+let CxC = document.querySelector('input#CxC')
+
+function iniciar(){
+    CxA.focus()
+    CxA.value = ''
+}
+
+function reseolver(){
+    
+}
