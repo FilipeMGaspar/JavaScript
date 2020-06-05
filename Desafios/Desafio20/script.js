589c092fae6d401e906e804ce542db1139cfc5b8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 17:19:11 +0100

    Desafio20 - Trabalhar com Datas.

--- Desafios/Desafio20/script.js
+++ Desafios/Desafio20/script.js
@@ -7,21 +7,36 @@ function FcSelecionar(){
     let ano = dataRecebida.slice(0,4)
     let mes = dataRecebida.slice(5,7)
     let dia = dataRecebida.slice(8,10)
-    let m = mes
-
-    switch (m){
-        case '01':
-            m = 'Janeiro'
-        break
-    }
-
-    
+    let m = mesSelecionado(mes)
 
     DivResultado.innerHTML = `<p>Data Selecionada ${dia}-${mes}-${ano}</p>`
     DivResultado.innerHTML +=`<p>Dia: ${dia}</p>`
     DivResultado.innerHTML += `<p>Mês: ${m}</p>`
     DivResultado.innerHTML += `<p>Ano: ${ano}</p>`
-   /* alert(`Ano selecionado ${ano}`)
-    alert(`Mês selecionado ${mes}`)
-    alert(`Dia Selecionado ${dia}`)*/
+}
+
+function mesSelecionado(mesInserido){
+    switch (mesInserido){
+        case '01':
+            return 'Janeiro'
+        break
+        
+        case '02':
+            return 'Fevereiro'
+        break
+
+        case '03':
+            return 'Março'
+        break
+
+        case '04':
+           return 'Abril'
+        break
+
+        case '05':
+           return 'Maio'
+        break
+        
+
+    }
 }
