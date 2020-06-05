7e3a195c8d27b0180096e1e07e458fd1dca66f05
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 17:21:09 +0100

    Desafio20 - Trabalhar com Datas

--- Desafios/Desafio20/script.js
+++ Desafios/Desafio20/script.js
@@ -36,7 +36,33 @@ function mesSelecionado(mesInserido){
         case '05':
            return 'Maio'
         break
-        
 
+        case '06':
+           return 'Junho'
+        break
+
+        case '07':
+           return 'Julho'
+        break
+       
+        case '08':
+           return 'Agosto'
+        break
+
+        case '09':
+           return 'Setembro'
+        break
+
+        case '10':
+           return 'Outubro'
+        break
+
+        case '11':
+           return 'Novembro'
+        break
+        
+        case '12':
+           return 'Dezembro'
+        break
     }
 }
