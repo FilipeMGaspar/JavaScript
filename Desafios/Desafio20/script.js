5abe0f3ae8410e05d6798f6ffa33d4a50ab3b42a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-05 16:58:52 +0100

    Desafio20 - Trabalhar com Datas.

--- Desafios/Desafio20/script.js
+++ Desafios/Desafio20/script.js
@@ -3,5 +3,7 @@ let CxData = document.querySelector('input#CxData')
 function FcSelecionar(){
     let anoRecebido = CxData.value;
 
-    alert(`Ano selecionado ${anoRecebido}`)
+    let ano = anoRecebido.slice(0,4);
+
+    alert(`Ano selecionado ${ano}`)
 }
