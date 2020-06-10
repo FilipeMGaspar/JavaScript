3c8fc4d72c434bb33c1f585380759109a964517b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-10 09:43:52 +0100

    Desafio21 - Ordenando Vetores (array)

--- Desafios/Desafio21/script.js
+++ Desafios/Desafio21/script.js
@@ -4,3 +4,13 @@ function iniciar(){
     CxValor.focus()
     CxValor.value=''
 }
+
+function adicionar(){
+
+    if(CxValor.value.length==0){
+        alert('!! [ERRO] !! Não foi indicado nenhum valor! ')
+        iniciar()
+    }else{
+        alert('Tudo ok Podemos continuar')
+    }
+}
