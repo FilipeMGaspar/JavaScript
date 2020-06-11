d84d19d3512f17275a7a5618c780be4847ae6448
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 12:59:58 +0100

    Desafio22- Inverter sequencia de caracteres

--- Desafios/Desafio22/script.js
+++ Desafios/Desafio22/script.js
@@ -16,5 +16,7 @@ function inverter(){
 }
 
 function inverteColocaNaLista(palavra){
-    alert('tudo ok Podemos continuar: '+palavra)
+    let intemOption = document.createElement('option')
+    intemOption.text = palavra.split('').reverse().join('') // função nativa do java script para inverter uma string (ordem de caracteres)
+    cSelPalavraInv.appendChild(intemOption)
 }
