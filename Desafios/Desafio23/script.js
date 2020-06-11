e118ef5665aadbd9005babf7d1e7338f297443a2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 20:39:07 +0100

    Desafio23 - Armazem de Fruta - Preços, descontos

--- Desafios/Desafio23/script.js
+++ Desafios/Desafio23/script.js
@@ -1,6 +1,7 @@
 let cSelDetalhes = document.querySelector('select#cSelDetalhes')
 let cxSubTot = document.querySelector('input#cxSubTot')
 let divResultado = document.querySelector('div#resultado')
+let totPagamento = []
 
 
 function addfruta(fruta){
@@ -21,7 +22,7 @@ function addfruta(fruta){
 function mostraDetalhes(qtd, frut){
     let itemOption = document.createElement('option')
     let preco = 0
-    
+
     itemOption.text =`${frut}   ${qtd}Kg`
     cSelDetalhes.appendChild(itemOption)
     preco = calculaPreco(qtd, frut)
@@ -29,22 +30,20 @@ function mostraDetalhes(qtd, frut){
     
 }
 
-function calculaPreco(qtdKg, qFruta){
-    let totPagamento = []
+function calculaPreco(qtdKg, qFruta){    
     let paga = 0
     let subtot = 0
 
     if ((qtdKg<5) && (qFruta==="Morango")){
         paga +=  2.50 * qtdKg
-        totPagamento.push(paga)
-       
-        alert (`Tot a Pagar ${paga}`)
-
-        for(pos in totPagamento){
-            subtot += totPagamento[pos]
-        }
-        
-        return subtot   
-        divResultado.innerHTML = `${totPagamento}`    
+        alert (`Tot a Pagar ${paga}`) 
     }
+    totPagamento.push(paga)
+
+    /*for(pos in totPagamento){
+        subtot += totPagamento[pos]
+    }*/
+
+    divResultado.innerHTML = `${totPagamento}` 
+    return subtot    
 }
