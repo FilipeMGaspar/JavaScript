50da5e31b6aea38200898d3c14fe3f4c026e1a99
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 20:02:10 +0100

    Desafio23 - Armazem de Fruta - Preços, descontos

--- Desafios/Desafio23/script.js
+++ Desafios/Desafio23/script.js
@@ -4,15 +4,17 @@ let cxSubTot = document.querySelector('input#cxSubTot')
 function addfruta(fruta){
     let qt = prompt("Quantos quilos?")
     let itemOption = document.createElement('option')
+    let preco = 0
+
     itemOption.text =`${fruta}   ${qt}Kg`
     cSelDetalhes.appendChild(itemOption)
 
     if ((qt<5) && (fruta==="Morango")){
         alert ('aki')
-        let preco = 2.50 * qt
+        preco +=  2.50 * qt
         alert (preco)
         cxSubTot.focus()
-        cxSubTot.value += `${preco}`
+        cxSubTot.value = `${preco}€`
     }
 
 }
