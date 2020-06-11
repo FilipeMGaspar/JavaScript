af8702a05a25ad61cce0fed0b1d81b26c24f2322
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-11 12:50:00 +0100

    Desafio22- Inverter sequencia de caracteres

--- Desafios/Desafio22/script.js
+++ Desafios/Desafio22/script.js
@@ -2,5 +2,15 @@ let cSelPalavraInv = document.querySelector('select#cSelPalavraInv')
 let Cxpalavra = document.querySelector('input#Cxpalavra')
 
 function iniciar(){
-    
+    Cxpalavra.focus()
+    Cxpalavra.value=''
+}
+
+function inverter(){
+    if(Cxpalavra.value.length==0){
+        alert('!![ERRO]!! Nenhuma palavra foi indicada!')
+        iniciar()
+    }else{
+        alert('tudo ok Podemos continuar')
+    }
 }
