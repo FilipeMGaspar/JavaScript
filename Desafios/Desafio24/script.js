af0305512a8324628355e06e51a184d34ce78557
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-15 20:31:22 +0100

    Desafio24 - MMC e MDC

--- Desafios/Desafio24/script.js
+++ Desafios/Desafio24/script.js
@@ -1,3 +1,17 @@
 let cxVal1 = document.querySelector('input#cxVal1')
 let CxVal2 = document.querySelector('input#CxVal2')
 
+function iniciar(){
+    cxVal1.focus()
+    cxVal1.value = ''
+}
+
+function calcular(){
+
+    if(cxVal1.value.length==0){
+        alert ('!![ERRO]!! Valor 1 não foi informado!')
+        iniciar()
+    }else{
+        alert ('Tudo ok podemos continuar')
+    }
+}
