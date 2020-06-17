7db612b7a7c05a6496f83151f8713d2b853ece9f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-17 17:55:55 +0100

    Desafio25 - Adicional_02 - Números Aleatórios

--- Desafios/Desafio25_adicional_02/script.js
+++ Desafios/Desafio25_adicional_02/script.js
@@ -1,6 +1,8 @@
 function gerar(){
     let num = 0
+    let res = document.querySelector('div#res')
 
     num = Math.random() * 50
-    
+
+    res.innerHTML = `<p>Número Aleatório: <strong>${num}</strong></p>`
 }
