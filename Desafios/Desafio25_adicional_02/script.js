c4d95cc673c9d9e405bf8b6d703849246d0d3bed
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-17 18:00:21 +0100

    Desafio25 - Adicional_02 - Números Aleatórios

--- Desafios/Desafio25_adicional_02/script.js
+++ Desafios/Desafio25_adicional_02/script.js
@@ -2,7 +2,7 @@ function gerar(){
     let num = 0
     let res = document.querySelector('div#res')
 
-    num = Math.floor(Math.random() * 50)
+    num = Math.floor(Math.random() * 50) // Math chamado floor(). Este método arredonda o número para baixo até o número inteiro mais próximo. Mesmo se o valor estiver acima de 0,5.
 
     res.innerHTML = `<p>Número Aleatório: <strong>${num}</strong></p>`
 }
