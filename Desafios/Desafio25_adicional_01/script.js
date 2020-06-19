14b4bb4de805698723692432ce4a95f3f96a3c2c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-19 09:08:22 +0100

    Desafio25 - Adicional_01 - Abre um pop-up com Mensagem

--- Desafios/Desafio25_adicional_01/script.js
+++ Desafios/Desafio25_adicional_01/script.js
@@ -1,6 +1,10 @@
 let res = document.querySelector('div#res')
 
+/*
+  Função criada em um ficheiro externo que mostra uma mensagem. 
+*/
 function msg(){
     alert ('Muito obrigado por clicar: AIA')
     res.innerHTML = '<p>Muito obrigado por clicar: AIA</p>'
 }
+
