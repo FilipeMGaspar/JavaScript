c96842df82d31b583f6117a0b06b297c2278d48f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-21 10:00:56 +0100

    Desafio25 - Adicional 06- Utilizar o DOM verificando o Focus

--- Desafios/Desafio25_adicional_06/script.js
+++ Desafios/Desafio25_adicional_06/script.js
@@ -1,6 +1,7 @@
 let cx1 = document.querySelector('div#cx1')
 let titCx1 = document.getElementById('titCx1')
 let cx2 = document.querySelector('div#cx2')
+let titCx2 = document.getElementById('titCx2')
 let mudacx1 = false
 let mudacx2 = false
 
@@ -16,8 +17,11 @@ function clicoucx1(){
 }
 
 function focuscx2(){
-    cx1.style.background = '#DCC9B6'
-    alert ('A Caixa 1 Perdeu o foco! \u{1F634}')
-    titCx1.innerHTML = `Perdi o Foco! \u{1F97A}`
-    mudacx1 = false
+    if(mudacx2 == false){
+        cx1.style.background = '#DCC9B6'
+        alert ('A Caixa 1 Perdeu o foco! \u{1F634}')
+        titCx1.innerHTML = `Perdi o Foco! \u{1F97A}`
+        mudacx1 = false
+        mudacx2 = true        
+    }
 }
