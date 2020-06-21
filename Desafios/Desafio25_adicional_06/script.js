23966d68b44e5ea7488ee3ae079b02035a29ceb5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-21 09:47:42 +0100

    Desafio25 - Adicional 06- Utilizar o DOM verificando o Focus

--- Desafios/Desafio25_adicional_06/script.js
+++ Desafios/Desafio25_adicional_06/script.js
@@ -1,9 +1,10 @@
 let cx1 = document.querySelector('div#cx1')
 let titCx1 = document.getElementById('titCx1')
 
-function clicou(){
+function clicoucx1(){
     
     cx1.style.background = '#F7D1CD'
     cx1.style.color = '#000000'
     titCx1.innerHTML = `Clicou! \u{1F929}`
+    alert ('Primeira caixa ativa! \u{1F914}')
 }
