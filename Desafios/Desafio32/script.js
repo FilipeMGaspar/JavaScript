91573f6c05c666851078d7760e63b3396791673f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-23 10:05:52 +0100

    Desafio32 - Mensagem com valores! - Passagem de parametros

--- Desafios/Desafio32/script.js
+++ Desafios/Desafio32/script.js
@@ -1,3 +1,11 @@
 function msg(val){
     alert (`Selecionou o número: ${val}`)
 }
+
+
+function mostraMsg(){
+    let Cxval = document.querySelector('input#Cxval')
+    alert (`Na caixa de testo estava o número ${Number(Cxval.value)}`)
+    Cxval.focus()
+    Cxval.value = ''
+}
