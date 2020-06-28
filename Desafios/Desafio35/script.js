b6cad710fdb7e80ae630188cf33933f2aae29ea4
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-28 09:29:07 +0100

    Desafio35 - Tabuadas

--- Desafios/Desafio35/script.js
+++ Desafios/Desafio35/script.js
@@ -1,10 +1,2 @@
 let CxSeltab = document.querySelector('select#CxSeltab')
 
-function iniciar(){
-    let intemOption = document.createElement('Option')
-    for(let i=1; i<=10; i++){
-        intemOption.text = `${i}`
-        CxSeltab.appendChild(intemOption)
-    }
-
-}
