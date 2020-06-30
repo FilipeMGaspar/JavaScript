79b0aa53ac0730b43888cbd8af589eee4a54a6bd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-06-30 09:45:21 +0100

    Desafio37 - Operaçõoes com Números

--- Desafios/Desafio37/script.js
+++ Desafios/Desafio37/script.js
@@ -47,7 +47,9 @@ function calculadora(nr1, nr2, op){
             break 
 
         case "/":
-            divResultado.innerHTML = `<p><span class='foco'>${nr1}</span> / <span class='foco'>${nr2}</span> = <span class='foco'>${nr1/nr2}</span> </p>`
+            let divisao = nr1/nr2
+            divisao = divisao.toFixed(2)
+            divResultado.innerHTML = `<p><span class='foco'>${nr1}</span> / <span class='foco'>${nr2}</span> = <span class='foco'>${divisao}</span> </p>`
             break     
     }
 }
