4dcbf1678e0acfa3a85f3f6571857af170f007be
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-07-04 11:22:28 +0100

    Desafios38 - Números Pares e Impares!

--- Desafios/Desafio38/script.js
+++ Desafios/Desafio38/script.js
@@ -70,4 +70,5 @@ function mostraResultados(lstPar, LstImpar){
 
     divResultado.innerHTML = `<h2>No total introduziu <span class='foco'>${vetValores.length}</span> números</h2>`
     divResultado.innerHTML += `<h3>Total de números pares: <span class='foco'>${lstPar.length}</span></h3>`
+    divResultado.innerHTML += `<h4></h4>`
 }
