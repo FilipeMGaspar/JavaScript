042376f2bae6c477299c8d80478e303075c40662
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-08-09 12:35:40 +0100

    Desafio40 - Pesquisar por nome num ARRAY

--- Desafios/Desafio40/script.js
+++ Desafios/Desafio40/script.js
@@ -6,3 +6,17 @@ function iniciar(){
     CxNome.focus()
     CxNome.value = ''
 }
+
+function inserir(){
+
+    if(CxNome.value.length==0){
+        alert ('!![ERRO]!! Não foi indicado nenhum nome!')
+        iniciar()
+    }else{
+        adicionarNaLista(CxNome.value)
+    }
+}
+
+function adicionarNaLista(nome){
+    
+}
